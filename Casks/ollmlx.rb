cde6c0cf8566a7a03bb7de0d8fc14a45afba6342
Darryl Morley <darrylmorley.uk@gmail.com>
2026-03-30 21:02:37 +0100

    Update sha256 checksum for ollmlx cask

--- Casks/ollmlx.rb
+++ Casks/ollmlx.rb
@@ -1,6 +1,6 @@
 cask "ollmlx" do
   version "0.1.0"
-  sha256 "9d7c99954288c2da70ec3563ec72dd65b384a049eff2d4aff7bcd5943f70b762"
+  sha256 "96115e616d3390a515d548c00be9a3d97400aa845a523724cd1a8e8916a1de5c"
 
   url "https://github.com/darrylmorley/ollmlx/releases/download/v#{version}/ollmlx-#{version}.dmg"
   name "ollmlx"
